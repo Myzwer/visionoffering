2b4a6bc48a863ee15e3dc352575b96894ba15b8b
Myzwer <josh@onefortyfivedesign.com>
2024-10-25 23:02:39 -0400

    :rocket: Changed the display banner to have a paragraph, as well as 4 wide with new icons.

--- components/FullBanner.vue
+++ components/FullBanner.vue
@@ -3,27 +3,55 @@
     <div class="mx-auto text-center lg:max-w-7xl py-5 px-5">
       <div class="grid grid-cols-12 gap-4 -ml-3 md:ml-0">
 
-        <div class="col-span-12 lg:col-span-4 opacity-20">
+		  <div class="col-span-12 text-left">
+			  <h2 class = "text-2xl">Impact Lanes</h2>
+			  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, asperiores autem consectetur dolore ea enim et eveniet officiis optio, quisquam reiciendis tempore temporibus velit veniam. Non perferendis repellendus sequi?</p>
+			  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias, asperiores autem consectetur dolore ea enim et eveniet officiis optio, quisquam reiciendis tempore temporibus velit veniam. Non perferendis repellendus sequi?</p>
+		  </div>
+
+        <div class="col-span-12 lg:col-span-3">
           <div class="box">
             <img
-              src="/images/icons/heart.png"
+              src="/images/icons/hammer.png"
               alt="icon"
               class="icon-width m-3"
             >
-            <span class="text-left"><strong>COMPLETED: Goal #1 - $94,500</strong> <br />Bless Outreach Minisries</span>
+            <span class="text-left"><strong>$1,200,000</strong> <br />FC Projects</span>
           </div>
         </div>
 
-        <div class="col-span-12 lg:col-span-4">
+        <div class="col-span-12 lg:col-span-3">
           <div class="box">
             <img
-				src="/images/icons/counsel.png"
+				src="/images/icons/map.png"
               alt="icon"
               class="icon-width m-3"
             >
-            <span class="text-left"><strong>CURRENT: Goal #2 - $1,200,000</strong> <br />Build Counseling Center </span>
+            <span class="text-left"><strong>$1,200,000</strong> <br />FC Multiplication </span>
           </div>
         </div>
+
+		  <div class="col-span-12 lg:col-span-3">
+			  <div class="box">
+				  <img
+					  src="/images/icons/group.png"
+					  alt="icon"
+					  class="icon-width m-3"
+				  >
+				  <span class="text-left"><strong>$1,200,000</strong> <br />Local Outreach </span>
+			  </div>
+		  </div>
+
+		  <div class="col-span-12 lg:col-span-3">
+			  <div class="box">
+				  <img
+					  src="/images/icons/globe.png"
+					  alt="icon"
+					  class="icon-width m-3"
+				  >
+				  <span class="text-left"><strong>$1,200,000</strong> <br />Global Outreach </span>
+			  </div>
+		  </div>
       </div>
     </div>
   </div>
